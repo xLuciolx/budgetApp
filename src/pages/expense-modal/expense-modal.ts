c81700da575a41383977d6153c23463a506ef31c
Loïc Gallay <lgallay@orange.fr>
2018-05-28 22:51:29 +0200

    Work on date

--- src/pages/expense-modal/expense-modal.ts
+++ src/pages/expense-modal/expense-modal.ts
@@ -11,45 +11,59 @@ import { Expense } from './../../models/Expense';
  */
 
 @Component({
-  selector: 'page-expense-modal',
-  templateUrl: 'expense-modal.html',
+    selector: 'page-expense-modal',
+    templateUrl: 'expense-modal.html',
 })
 export class ExpenseModalPage {
 
-  date = new Date();
-  expForm: FormGroup;
-  title: string = 'TL_ADD_EXPENSE';
-  id: string;
-  month: string;
-  year: string;
+    date = new Date();
+    expForm: FormGroup;
+    title: string = 'TL_ADD_EXPENSE';
+    today: Date
+    lastDay
+    month
+    min
+    max
 
-  constructor(
-    private fb: FormBuilder,
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public viewCtrl: ViewController
-  ) {
-    this.createForm();
-    this.id = navParams.get('id');
-    Number(this.id.slice(0, -4)) < 10 ? this.month = '0' + (Number(this.id.slice(0, -4)) + 1) : this.month = this.id.slice(0, -4) + 1;
-    this.year = this.id.substr(this.id.length - 4);
-  }
+    constructor(
+        private fb: FormBuilder,
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public viewCtrl: ViewController
+    ) {
+        this.today = new Date()
+        // this.lastDay = new Date(this.today.getFullYear(), this.today.getMonth() + 1, 0)
+        // console.log(this.lastDay);
 
-  createForm() {
-    this.expForm = this.fb.group({
-      name: ['', Validators.required],
-      person: ['', Validators.required],
-      amount: ['', Validators.required],
-      date: ['', Validators.required]
-    });
-  }
+        this.today.getMonth() < 10 ? this.month = '0' + (this.today.getMonth() + 1) : this.month = (this.today.getMonth() + 1)
+        let maxDay = this.daysInMonth(this.today.getMonth(), this.today.getFullYear())
+        console.log(maxDay);
 
-  addExpense(data) {
-    let expanse = new Expense(data);
-    this.viewCtrl.dismiss(expanse);
-  }
+        this.min = new Date(this.today.getFullYear() + '-' + this.month).toISOString();
+        this.max = new Date(this.today.getFullYear() + '-' + this.today.getMonth() + '-' + maxDay).toISOString()
+        this.createForm();
+    }
 
-  dismiss() {
-    this.viewCtrl.dismiss();
-  }
+
+    daysInMonth(month, year) {
+        return new Date(year, month, 0).getDate()
+    }
+
+    createForm() {
+        this.expForm = this.fb.group({
+            name: ['', Validators.required],
+            person: ['', Validators.required],
+            amount: ['', Validators.required],
+            date: ['', Validators.required]
+        });
+    }
+
+    addExpense(data) {
+        let expanse = new Expense(data);
+        this.viewCtrl.dismiss(expanse);
+    }
+
+    dismiss() {
+        this.viewCtrl.dismiss();
+    }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,120 +7,120 @@ import { ExpenseModalPage } from '../expense-modal/expense-modal';
 
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  title: string = 'TL_APP';
-  month: Month;
-  id: string;
-  date = new Date();
-  totalExpenses: number;
+    title: string = 'TL_APP';
+    month: Month;
+    id: string;
+    date = new Date();
+    totalExpenses: number;
 
 
 
-  constructor(
-    private dbService: dbService,
-    public translate: TranslateService,
-    public navCtrl: NavController,
-    public alertCtrl: AlertController,
-    public modalCtrl: ModalController
-  ) {
-  }
+    constructor(
+        private dbService: dbService,
+        public translate: TranslateService,
+        public navCtrl: NavController,
+        public alertCtrl: AlertController,
+        public modalCtrl: ModalController
+    ) {
+    }
 
-  ionViewDidLoad() {
-    console.log(new Date());
+    ionViewDidLoad() {
+        console.log(new Date());
 
-    this.id = this.date.getMonth().toString() + this.date.getFullYear().toString();
-    this.dbService.getCurrentMonthInfos(this.id).then(
-      result => {
-        this.month = result
-        this.checkIncome();
+        this.id = this.date.getMonth().toString() + this.date.getFullYear().toString();
+        this.dbService.getCurrentMonthInfos(this.id).then(
+            result => {
+                this.month = result
+                this.checkIncome();
+                // console.log(this.month);
+            }
+        )
+    }
+
+    checkIncome() {
+        if (!this.month.income) {
+            this.incomeAlert().then(
+                () => {
+                    if (this.month.income) this.calculs()
+                    else this.checkIncome()
+                },
+                err => this.checkIncome()
+            )
+        }
+        else this.calculs()
+    }
+
+    async calculs() {
         // console.log(this.month);
-      }
-    )
-  }
-
-  checkIncome() {
-    if (!this.month.income) {
-      this.incomeAlert().then(
-        () => {
-          if (this.month.income) this.calculs()
-          else this.checkIncome()
-        },
-        err => this.checkIncome()
-      )
+        if (!this.month.expenses) {
+            this.month.balance = this.month.income
+        } else {
+            this.totalExpenses = this.month.totalExpenses();
+            this.month.balance = this.month.income - this.totalExpenses
+        }
     }
-    else this.calculs()
-  }
 
-  async calculs() {
-    // console.log(this.month);
-    if (!this.month.expenses) {
-      this.month.balance = this.month.income
-    } else {
-      this.totalExpenses = this.month.totalExpenses();
-      this.month.balance = this.month.income - this.totalExpenses
-    }
-  }
-
-  async incomeAlert(): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-      this.dbService.getCurrentMonthInfos(this.id).then(
-        month => {
-          this.month = month
-          let income = this.alertCtrl.create({
-            title: this.translate.instant('TL_YOUR_INCOME'),
-            inputs: [
-              {
-                name: 'income',
-                type: 'number'
-              }
-            ],
-            buttons: [
-              {
-                text: this.translate.instant('TL_CANCEL'),
-                role: 'cancel',
-                handler: () => resolve()
-              },
-              {
-                text: this.translate.instant('TL_VALID'),
-                role: 'save',
-                handler: data => {
-                  if (data.income) {
-                    this.month.income = data.income
-                    this.dbService.save(this.month).then(
-                      result => {
-                        this.month = result
-                        resolve()
-                      },
-                      err => reject()
-                    )
-                  }
+    async incomeAlert(): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.dbService.getCurrentMonthInfos(this.id).then(
+                month => {
+                    this.month = month
+                    let income = this.alertCtrl.create({
+                        title: this.translate.instant('TL_YOUR_INCOME'),
+                        inputs: [
+                            {
+                                name: 'income',
+                                type: 'number'
+                            }
+                        ],
+                        buttons: [
+                            {
+                                text: this.translate.instant('TL_CANCEL'),
+                                role: 'cancel',
+                                handler: () => resolve()
+                            },
+                            {
+                                text: this.translate.instant('TL_VALID'),
+                                role: 'save',
+                                handler: data => {
+                                    if (data.income) {
+                                        this.month.income = data.income
+                                        this.dbService.save(this.month).then(
+                                            result => {
+                                                this.month = result
+                                                resolve()
+                                            },
+                                            err => reject()
+                                        )
+                                    }
+                                }
+                            }
+                        ]
+                    })
+                    income.present()
                 }
-              }
-            ]
-          })
-          income.present()
-        }
-      )
-    })
-  }
+            )
+        })
+    }
 
-  openModal() {
-    let modal = this.modalCtrl.create(ExpenseModalPage, { id: this.id });
-    modal.onDidDismiss(data => {
-      if (!data) return false
-      if (!this.month.expenses) this.month.expenses = []
-      this.month.expenses.push(data)
-      this.dbService.save(this.month).then(
-        result => {
-          this.month = result
-          this.calculs()
-        }
-      )
-    })
-    modal.present();
-  }
+    openModal() {
+        let modal = this.modalCtrl.create(ExpenseModalPage);
+        modal.onDidDismiss(data => {
+            if (!data) return false
+            if (!this.month.expenses) this.month.expenses = []
+            this.month.expenses.push(data)
+            this.dbService.save(this.month).then(
+                result => {
+                    this.month = result
+                    this.calculs()
+                }
+            )
+        })
+        modal.present();
+    }
 }
